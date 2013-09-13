886c0b987d0bc8fa010c2f9cb88304e1522b96f0
Aditya Viswanathan <aditya@adityaviswanathan.com>
2013-09-12 18:57:42 -0700

    initialized rails

---
+++ av-site_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AvSiteApp::Application.config.secret_token = '93a8a35f4c9db6f9b7f41949403227fcbfe2c0d085e5a457f7aaa82fffa64f722a53613fa85413aa2850e62e6efa8eb9ddde87e063876abc2d6bd6089fe5c86f'
